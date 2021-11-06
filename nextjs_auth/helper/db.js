6dc08c349609bf56cfc340f339de324d7d4163ef
tengfone <phangtf@gmail.com>
2021-11-06 16:21:52 +0800

    Fix

--- nextjs_auth/helper/db.js
+++ nextjs_auth/helper/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    "mongodb+srv://tengfone:Whatever1!@cluster0.gseq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://XXXX"
   );
   return client;
 }
